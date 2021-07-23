3a73cb9788436f5f663cb5431683ba8a11e01f18
Lucas Corte <corte1994.lc@gmail.com>
2021-07-23 15:13:19 -0300

    transformação do objeto em uma model

--- models/hotel.py
+++ models/hotel.py
@@ -1,4 +1,14 @@
-class HotelModel:
+from create_db import db
+
+class HotelModel(db.Model):
+  __tablename__ = 'hoteis'
+
+  id = db.Column(db.String, primary_key=True)
+  name = db.Column(db.String(80))
+  stars = db.Column(db.Float(precision=1))
+  daily = db.Column(db.Float(precision=2))
+  city = db.Column(db.String(40))
+
   def __init__(self, id, name, stars, daily, city):
     self.id = id
     self.name = name
