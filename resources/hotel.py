a7bed049a1507cb7117379783140ae8af78d5f5c
Lucas Corte <corte1994.lc@gmail.com>
2021-07-23 14:03:43 -0300

    implementaçao a model de hoteis

--- resources/hotel.py
+++ resources/hotel.py
@@ -24,6 +24,25 @@ hoteis = [
   },
 ]
 
+
+class HotelModel:
+  def __init__(self, id, name, stars, daily, city):
+    self.id = id
+    self.name = name
+    self.stars = stars
+    self.daily = daily
+    self.city = city
+
+  def json(self):
+    return {
+      "id": self.id,
+      "name": self.name,
+      "stars": self.stars,
+      "daily": self.daily,
+      "city": self.city
+    }
+
+
 class Hoteis(Resource):
   def get(self):
     return {'hoteis': hoteis}
